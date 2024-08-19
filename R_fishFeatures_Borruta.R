7d25cd223deccee948c27559cf656a8d5c50654a
mariewntr <mariewntr@gmail.com>
2024-08-19 10:05:43 +0200

    try different regression models not very work can't go over 0.3 r_squared performance because of overfitting the data.

--- R_fishFeatures_Borruta.R
+++ R_fishFeatures_Borruta.R
@@ -7,8 +7,8 @@ library(Boruta)
 #feature engeneering by sushmita + select at the end of the report from all
 data = read.csv("C:/Users/marie/Documents/DSTI_Cours/Python_Class/Python_Lab/Exam_project/Project 2/CoralReefFishSpeciesAssessment/df_ML.csv")
 
-#original dataset with missing values
-data = read.csv("C:/Users/marie/Documents/DSTI_Cours/Python_Class/Python_Lab/Exam_project/Project 2/CoralReefFishSpeciesAssessment/Data/species.csv")
+#2nd dataset
+data = read.csv("C:/Users/marie/Documents/DSTI_Cours/Python_Class/Python_Lab/Exam_project/Project 2/CoralReefFishSpeciesAssessment/species_ML_2.csv")
 
 #adding the cat column
 data$Diversity_cat = ifelse(
@@ -17,19 +17,6 @@ data$Diversity_cat = ifelse(
          ifelse(data$Diversity_index >= 0.9, 'high', '0'))
 )
 
-data2 <- data %>%
-  select(Depth, Region, Coral_cover, Reef_complexity, Coral_area_UFRTM_20km, Coral_area_UFRTM_200km,
-         SST, NPP, Wave_exposure, Habitat_type_classLV0, Habitat_type_classLV2, Depth_Sbrocco, Deepwater, FSA,
-         Marina_slips_10km, Marina_slips_25km, Marine_reserve, Population_20km, Population_50km, Pop_per_area_reef_20km,
-         Recreational_fishermen_50km, Tourist_fishing, Artificial_reefs_1km, SG_permits_50km, SG_charter_permits_25km, Total_gravity, 
-         Nursery_seagrass, Nursery_mangroves, connectivity, Comm_engagement, Comm_reliance, Commercial_pounds_landed, 
-         Rec_reliance, Rec_engagement, impact, Diversity_index)
-
-data_borruta2 <- transform(data2,
-                          Region=as.factor(Region),
-                          Habitat_type_classLV0= as.factor(Habitat_type_classLV0), 
-                          Habitat_type_classLV2=as.factor(Habitat_type_classLV2),
-                          Marine_reserve=as.factor(Marine_reserve))
 
 data_borruta <- transform(data,
                           Region=as.factor(Region),
@@ -37,7 +24,7 @@ data_borruta <- transform(data,
 
 borruta_output <- Boruta(Diversity_cat~., data= data_borruta, doTrace=2)
 
-borruta_output2 <- Boruta(Diversity_index~., data= data_borruta2, doTrace=2)
+borruta_output2 <- Boruta(Diversity_index~., data= data, doTrace=2)
 
 print(borruta_output)
 plot(borruta_output, xlab = "", xaxt = "n")
@@ -47,4 +34,16 @@ names(k) <- colnames(borruta_output$ImpHistory)
 Labels <- sort(sapply(k,median))
 axis(side = 1, las=2, labels = names(Labels),
      at = 1:ncol(borruta_output$ImpHistory), cex.axis=0.7)
+
+
+print(borruta_output2)
+plot(borruta_output2, xlab = "", xaxt = "n")
+k <- lapply(1:ncol(borruta_output2$ImpHistory), function(i)
+  borruta_output2$ImpHistory[is.finite(borruta_output2$ImpHistory[,i]),i])
+names(k) <- colnames(borruta_output2$ImpHistory)
+Labels <- sort(sapply(k,median))
+axis(side = 1, las=2, labels = names(Labels),
+     at = 1:ncol(borruta_output2$ImpHistory), cex.axis=0.7)
+
+
 
